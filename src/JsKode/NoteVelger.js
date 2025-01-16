e0ef8af9f77d3cf9b059cbec427f139cf753b277
Aksel-Reistad <aksrei04@innlandetfylke.no>
2025-01-16 09:51:55 +0100

    organisert ted og knap for å se bokstaver

---
+++ src/JsKode/NoteVelger.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoteVelger({ SyneligBokstaver }) {
+  return (
+    <div>
+      {/* <button onClick={SyneligBokstaver}>Knap</button> */}
+    </div>
+  );
+}
+
+export default NoteVelger;
